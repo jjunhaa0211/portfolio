6bef5725b2e309a3f394efca87527014dcd48845
Mooner510 <lmck510@gmail.com>
2023-12-10 05:33:55 +0900

    add more route & header split

---
+++ src/_portfolio/components/Header.tsx
@@ -0,0 +1,78 @@
+import { Txt } from "../../components/Txt";
+import styled from "styled-components";
+import { motions } from "../framer/motions";
+import { useMediaQuery } from "react-responsive";
+import { NavigateFunction, NavLink, Outlet, useNavigate, useNavigation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export const toMain = (nav: NavigateFunction) => {
+    const priv = localStorage.getItem("prev")
+    if (priv && priv === "/") nav(-1);
+    else nav("/");
+}
+
+const Header = () => {
+    const media = useMediaQuery({ query: "(min-width: 768px)" });
+    const nav = useNavigate();
+
+    useEffect(() => {
+        localStorage.setItem("prev", localStorage.getItem("cur") ?? "");
+        localStorage.setItem("cur", location.pathname);
+        console.log("head");
+    }, [])
+
+    return <>
+        <Box
+            initial={{
+                translateY: "-50px"
+            }}
+            animate={{
+                translateY: "0px"
+            }}
+            transition={{
+                duration: 1,
+                ease: [0.22, 1, 0.36, 1]
+            }}
+        >
+            <HeaderInner>
+                <Txt typography={"H4"}
+                     onClick={() => toMain(nav)}
+                     clickable>Mooner510</Txt>
+            </HeaderInner>
+            {media && <>
+                <Txt typography={"P1"}>Home</Txt>
+                <Txt typography={"P1"}>About</Txt>
+                <Txt typography={"P1"}>Project</Txt>
+                <Txt typography={"P1"}>Contact</Txt>
+            </>}
+        </Box>
+        <Outlet/>
+    </>
+}
+
+const HeaderInner = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  flex: 1 0 0;
+`
+
+const Box = styled(motions.fadeDiv)`
+  position: fixed;
+  z-index: 100;
+  width: calc(100vw - 320px);
+  height: 50px;
+  display: flex;
+  padding: 20px 160px;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 60px;
+  backdrop-filter: blur(6px);
+
+  @media (max-width: 768px) {
+    width: calc(100vw - 160px);
+    padding: 20px 80px;
+  }
+`
+
+export default Header;
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,13 +3,22 @@ import '../i18n/config';
 import { Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import styled from "styled-components";
+import Header from "../_portfolio/components/Header";
+import ProjectPage from "../_portfolio/pages/ProjectPage";
+import SkyblockRemake from "../_portfolio/pages/portfolio/SkyblockRemake";
 
 export const APP_VERSION = "v0.1-231216";
 
 function App() {
     return (
         <Routes>
-            <Route path='/' element={<Main/>}/>
+            <Route element={<Header/>}>
+                <Route path='/:irl' element={<Main/>}/>
+                <Route element={<ProjectPage/>}>
+                    <Route path={"/project/skyblock"} element={<SkyblockRemake/>}/>
+                </Route>
+                <Route path='/*' element={<Main/>}/>
+            </Route>
         </Routes>
     );
 }
