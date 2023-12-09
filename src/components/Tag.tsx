bdd188ae5ffd07162a6e055bc39ec3be39ddb27e
Mooner510 <lmck510@gmail.com>
2023-12-10 05:35:49 +0900

    text & tag fix and update

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -15,17 +15,19 @@ const Tag = (props: TagProps) => {
 };
 
 export const DefaultTag = {
-    unity: <Tag name={"Unity"} backgroundColor={"#767676"} textColor={"White0"}/>,
-    springBoot: <Tag name={"SpringBoot"} backgroundColor={"#77bc1f"} textColor={"Black0"}/>,
-    android: <Tag name={"Android"} backgroundColor={"#319d46"} textColor={"White0"}/>,
-    react: <Tag name={"React.js"} backgroundColor={"#61dafb"} textColor={"Black0"}/>,
-    spigot: <Tag name={"Spigot"} backgroundColor={"#b4842c"} textColor={"White0"}/>,
-    csharp: <Tag name={"C#"} backgroundColor={"#0A9910"} textColor={"White0"}/>,
-    java: <Tag name={"Java"} backgroundColor={"#CA7010"} textColor={"White0"}/>,
-    kotlin: <Tag name={"Kotlin"} backgroundColor={"#A930E7"} textColor={"White0"}/>,
-    html: <Tag name={"HTML/CSS"} backgroundColor={"#E75212"} textColor={"White0"}/>,
-    javaScript: <Tag name={"JavaScript"} backgroundColor={"#F4DC1C"} textColor={"Black0"}/>,
-    typeScript: <Tag name={"TypeScript"} backgroundColor={"#55A5FF"} textColor={"Black0"}/>,
+    unity: <Tag key={"Unity"} name={"Unity"} backgroundColor={"#767676"} textColor={"White0"}/>,
+    springBoot: <Tag key={"SpringBoot"} name={"SpringBoot"} backgroundColor={"#77bc1f"} textColor={"Black0"}/>,
+    android: <Tag key={"Android"} name={"Android"} backgroundColor={"#319d46"} textColor={"White0"}/>,
+    react: <Tag key={"React"} name={"React.js"} backgroundColor={"#61dafb"} textColor={"Black0"}/>,
+    spigot: <Tag key={"Spigot"} name={"Spigot"} backgroundColor={"#b4842c"} textColor={"White0"}/>,
+    csharp: <Tag key={"C#"} name={"C#"} backgroundColor={"#0A9910"} textColor={"White0"}/>,
+    java: <Tag key={"Java"} name={"Java"} backgroundColor={"#CA7010"} textColor={"White0"}/>,
+    kotlin: <Tag key={"Kotlin"} name={"Kotlin"} backgroundColor={"#A930E7"} textColor={"White0"}/>,
+    html: <Tag key={"HTML"} name={"HTML/CSS"} backgroundColor={"#E75212"} textColor={"White0"}/>,
+    javaScript: <Tag key={"TypeScript"} name={"JavaScript"} backgroundColor={"#F4DC1C"} textColor={"Black0"}/>,
+    typeScript: <Tag key={"JavaScript"} name={"TypeScript"} backgroundColor={"#55A5FF"} textColor={"White0"}/>,
+    sql: <Tag key={"SQL"} name={"SQL"} backgroundColor={"#767676"} textColor={"White0"}/>,
+    sqlite: <Tag key={"SQLite"} name={"SQLite"} backgroundColor={"#5799fd"} textColor={"White0"}/>,
 }
 
 const Box = styled.div<{ color: string }>`
--- src/components/Txt.tsx
+++ src/components/Txt.tsx
@@ -3,19 +3,23 @@ import { FontStyles, keyOfFontStyles } from "../styles/font";
 import { Colors, keyOfColors } from "../styles/color";
 import styled from "styled-components";
 import { Animations } from "../styles/animations";
+import { motion, MotionProps } from "framer-motion";
 
-interface Props extends HTMLAttributes<HTMLSpanElement> {
+type Props = HTMLAttributes<HTMLSpanElement> & MotionProps & {
     typography?: keyOfFontStyles;
     style?: CSSProperties;
     color?: keyOfColors;
     nativeColor?: string;
     width?: string;
     height?: string;
+    margin?: string;
     anim?: boolean;
     clickable?: boolean;
     center?: boolean;
     right?: boolean;
     left?: boolean;
+    justify?: boolean;
+    $lineheight?: string;
 }
 
 export const Txt = (
@@ -27,56 +31,44 @@ export const Txt = (
         width,
         height,
         center,
+        margin,
         right,
         left,
         anim,
+        justify,
         clickable,
+        $lineheight,
         ...props
     }: Props) => {
+    const innerStyle: CSSProperties = {
+        color: Colors[color],
+        width: width,
+        height: height,
+        textAlign: justify ? 'justify' : center ? 'center' : right ? 'right' : left ? 'left' : 'unset',
+        cursor: clickable ? "pointer" : "default",
+        margin: margin ?? "0",
+        ...FontStyles[typography],
+        ...style,
+    };
+
     return (
-        nativeColor ?
-            <TextNativeSpan
-                style={{
-                    color: Colors[color],
-                    width: width,
-                    height: height,
-                    textAlign: center ? 'center' : right ? 'right' : left ? 'left' : 'unset',
-                    cursor: clickable ? "pointer" : "default",
-                    ...FontStyles[typography],
-                    ...style,
-                }}
-                $background={nativeColor}
-                anim={anim}
-                {...props}
-            /> :
-            <TextSpan
-                style={{
-                    color: Colors[color],
-                    width: width,
-                    height: height,
-                    textAlign: center ? 'center' : right ? 'right' : left ? 'left' : 'unset',
-                    cursor: clickable ? "pointer" : "default",
-                    ...FontStyles[typography],
-                    ...style,
-                }}
-                anim={anim}
-                {...props}
-            />
+        nativeColor ? <TextNativeSpan style={innerStyle} $background={nativeColor} $lineheight={$lineheight} anim={anim} {...props}/> :
+            <TextSpan style={innerStyle} $lineheight={$lineheight} anim={anim} {...props}/>
     );
 };
 
-const TextSpan = styled.span<{ anim?: boolean }>`
+const TextSpan = styled(motion.span)<{ $lineheight?: string, anim?: boolean }>`
   margin: 0;
   padding: 0;
-  line-height: 1.2;
+  line-height: ${props => props.$lineheight ?? "1.2"};
   opacity: ${props => props.anim ? 0 : 1};
   animation: ${props => props.anim ? "1s cubic-bezier(0.22, 1, 0.36, 1) ${Animations.showAnim(\"-6px\")} forwards" : undefined};
 `
 
-const TextNativeSpan = styled.span<{ anim?: boolean, $background: string }>`
+const TextNativeSpan = styled(motion.span)<{ $lineheight?: string, anim?: boolean, $background: string }>`
   margin: 0;
   padding: 0;
-  line-height: 1.2;
+  line-height: ${props => props.$lineheight ?? "1.2"};
   opacity: ${props => props.anim ? 0 : 1};
   background: ${props => props.$background};
   background-clip: text;
