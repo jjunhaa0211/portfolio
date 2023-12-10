f93c3f4a3c0297fa887f084e536de8d32adecac6
Mooner510 <lmck510@gmail.com>
2023-12-10 20:26:42 +0900

    added new projects

--- src/_portfolio/components/Project.tsx
+++ src/_portfolio/components/Project.tsx
@@ -1,5 +1,5 @@
 import { Colors, keyOfColors } from "../../styles/color";
-import Tag, { keyOfDefaultTag, TagProps } from "../../components/Tag";
+import Tag, { DefaultTag, keyOfDefaultTag, TagBox, TagProps } from "../../components/Tag";
 import styled from "styled-components";
 import { keyOfIcons } from "../../styles/icons";
 import { GIcon } from "../../components/GIcon";
@@ -8,38 +8,46 @@ import { useMediaQuery } from "react-responsive";
 import { motions } from "../framer/motions";
 import { useState } from "react";
 import { animate, useAnimate, useAnimation } from "framer-motion";
-import { Background, DisplayDiv } from "./layout/projectLayout";
+import { useNavigate } from "react-router-dom";
 
-const Project = (props: { icon: keyOfIcons, name: string, date: string, tags: keyOfDefaultTag[], description: string }) => {
+const Project = (props: { icon: keyOfIcons, name: string, date: string, tags: keyOfDefaultTag[], description: string, link: string }) => {
     const media = useMediaQuery({ query: "(min-width: 768px)" });
     const anim = useAnimation()
+    const nav = useNavigate();
     const [show, setShow] = useState<boolean>(false);
 
-    return <TopBox show={{ backgroundColor: Colors.Black1 }} hide={{ backgroundColor: Colors.Black0 }} animate={anim}>
+    return <TopBox show={{ backgroundColor: "#00000066" }} hide={{ backgroundColor: "#00000000" }} animate={anim}>
         <Box
             y={35}
+            onClick={() => {
+                console.log(props.link)
+                nav(props.link)
+            }}
             onMouseOver={() => {
+                if (!media) return;
                 setShow(true)
                 anim.start("show");
             }}
             $show={show}>
-            <GIcon icon={props.icon} width={media ? "120px" : "80px"} height={media ? "120px" : "80px"} css={{ borderRadius: "12px" }}/>
+            <GIcon icon={props.icon} width={media ? "120px" : "60px"} height={media ? "120px" : "60px"} css={{ borderRadius: "12px" }}/>
             <DetailBox>
                 <TitleBox>
-                    <Txt typography={"H3"}>{props.name}</Txt>
+                    <Txt typography={media ? "H3" : "H4"}>{props.name}</Txt>
                     <Txt typography={"P1"}>{props.date}</Txt>
                 </TitleBox>
                 <TagBox>
-                    {props.tags}
+                    {props.tags.map(v => DefaultTag[v])}
                 </TagBox>
+                {!media && <Txt>{props.description}</Txt>}
             </DetailBox>
         </Box>
-        <DisplayDiv $display={show}>
+        {media && <DisplayDiv $display={show}>
             <Background
                 animate={anim}
                 show={{ opacity: 1 }}
                 hide={{ opacity: 0 }}
                 onMouseOver={() => {
+                    if (!media) return;
                     anim.start("hide");
                     setTimeout(() => {
                         setShow(false)
@@ -62,16 +70,31 @@ const Project = (props: { icon: keyOfIcons, name: string, date: string, tags: ke
                 <Txt center>{props.description}</Txt>
                 <div style={{ height: "5px" }}/>
             </SubTitle>
-        </DisplayDiv>
+        </DisplayDiv>}
     </TopBox>;
 };
+
+const Background = styled(motions.keyDiv)`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 10;
+  backdrop-filter: blur(4px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const DisplayDiv = styled.div<{ $display: boolean }>`
+  display: ${props => props.$display ? "block" : "none"};
+`
 
 const TopBox = styled(motions.keyDiv)`
   width: 570px;
   height: 220px;
   cursor: pointer;
-  background-color: ${Colors.Black0};
-  transition: all 0.3s;
   border-radius: 20px;
 
   * {
@@ -79,7 +102,8 @@ const TopBox = styled(motions.keyDiv)`
   }
 
   @media (max-width: 768px) {
-    width: 430px;
+    width: calc(100vw - 25px);
+    height: auto;
   }
 `
 
@@ -93,18 +117,6 @@ const SubTitle = styled(motions.keyDiv)<{ $down: boolean }>`
   align-items: center;
   flex-direction: column;
   justify-content: ${props => props.$down ? "flex-end" : "flex-start"};
-
-  @media (max-width: 768px) {
-    width: 400px;
-    padding: 0 15px;
-  }
-`
-
-const TagBox = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  align-self: stretch;
 `
 
 const TitleBox = styled.div`
@@ -127,7 +139,7 @@ const DetailBox = styled.div`
 
 const Box = styled(motions.fadeDiv)<{ $show: boolean }>`
   position: ${props => props.$show ? "absolute" : "unset"};
-  z-index: 20;
+  z-index: 50;
   display: flex;
   width: 500px;
   height: 220px;
@@ -145,8 +157,11 @@ const Box = styled(motions.fadeDiv)<{ $show: boolean }>`
   //}
 
   @media (max-width: 768px) {
-    width: 400px;
-    padding: 0 15px;
+    width: calc(100% - 30px);
+    height: calc(100% - 30px);
+    //min-width: calc(100% - 30px);
+    //gap: 5px;
+    padding: 10px;
   }
 `
 
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -6,6 +6,9 @@ import styled from "styled-components";
 import Header from "../_portfolio/components/Header";
 import ProjectPage from "../_portfolio/pages/ProjectPage";
 import SkyblockRemake from "../_portfolio/pages/portfolio/SkyblockRemake";
+import RicePaper from "../_portfolio/pages/portfolio/RicePaper";
+import Lite24 from "../_portfolio/pages/portfolio/Lite24";
+import BeatTable from "../_portfolio/pages/portfolio/BeatTable";
 
 export const APP_VERSION = "v0.1-231216";
 
@@ -16,6 +19,11 @@ function App() {
                 <Route path='/:irl' element={<Main/>}/>
                 <Route element={<ProjectPage/>}>
                     <Route path={"/project/skyblock"} element={<SkyblockRemake/>}/>
+                    <Route path={"/project/rice-paper"} element={<RicePaper/>}/>
+                    <Route path={"/project/lite24"} element={<Lite24/>}/>
+                    <Route path={"/project/beat-table"} element={<BeatTable/>}/>
+                    <Route path={"/project/genshin-calculator"} element={<RicePaper/>}/>
+                    <Route path={"/project/survival-from-the-epidemic"} element={<RicePaper/>}/>
                 </Route>
                 <Route path='/*' element={<Main/>}/>
             </Route>
