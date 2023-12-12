c76c38d1984cf6387e68ccfee294599892466be3
Mooner510 <lmck510@gmail.com>
2023-12-12 11:53:50 +0900

    fix all

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,38 @@ import { Global } from './styles/GlobalStyle';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { HelmetProvider } from "react-helmet-async";
+import { hydrate, render } from "react-dom";
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Analytics/>
-            <SpeedInsights/>
-            <Global/>
-            <HelmetProvider>
-                <App/>
-            </HelmetProvider>
-        </BrowserRouter>
-    </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+const element = <React.StrictMode>
+    <BrowserRouter>
+        <Analytics/>
+        <SpeedInsights/>
+        <Global/>
+        <HelmetProvider>
+            <App/>
+        </HelmetProvider>
+    </BrowserRouter>
+</React.StrictMode>
+
+if (rootElement?.hasChildNodes()) {
+    hydrate(element, rootElement);
+} else {
+    render(element, rootElement);
+}
+
+// const root = ReactDOM.createRoot(
+//     rootElement as HTMLElement
+// );
+// root.render(
+//     <React.StrictMode>
+//         <BrowserRouter>
+//             <Analytics/>
+//             <SpeedInsights/>
+//             <Global/>
+//             <HelmetProvider>
+//                 <App/>
+//             </HelmetProvider>
+//         </BrowserRouter>
+//     </React.StrictMode>
+// );
