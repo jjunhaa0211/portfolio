bdcb6b787701710c041c9701bdf02c4df95f83c8
Mooner510 <lmck510@gmail.com>
2023-12-12 12:02:14 +0900

    update header helmet

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './routes/App';
 import { BrowserRouter } from "react-router-dom";
 import { Global } from './styles/GlobalStyle';
